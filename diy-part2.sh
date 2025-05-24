646da5105db6a1c8973ac642ccab3b8c1cb673d2
mytest667 <xdhasang0912@outlook.com>
2025-05-24 10:05:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,16 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 删除自带 golang
+rm -rf feeds/packages/lang/golang
+# 拉取 golang
+git clone https://github.com/sbwml/packages_lang_golang.git -b 24.x feeds/packages/lang/golang
+
+# 删除自带 xray-core
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+
+# 删除自带 v2ray-core
+rm -rf feeds/packages/net/v2ray-core
+rm -rf package/feeds/packages/v2ray-core
