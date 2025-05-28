6692cc2d27083d5d065582db4bfb0567437e925f
mytest667 <xdhasang0912@outlook.com>
2025-05-28 22:04:01 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,27 +10,6 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# 删除自带 golang
-rm -rf feeds/packages/lang/golang
-# 拉取 golang
-git clone https://github.com/sbwml/packages_lang_golang.git -b 24.x feeds/packages/lang/golang
-
-# 删除自带 xray-core
-rm -rf feeds/packages/net/xray-core
-rm -rf package/feeds/packages/xray-core
-
-# 删除自带 v2ray-core
-rm -rf feeds/packages/net/v2ray-core
-rm -rf package/feeds/packages/v2ray-core
-
-# rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-# https://www.dropbox.com/scl/fo/16p7qk85j7gm92me98tlj/AHVhIZ10WM4MRwoATkn1E8I?rlkey=2rah1q0kib5hhf9lxngzo8qhm
+ tar -xvf owrt2410.tar -C openwrt
+ cd openwrt
+ sh R7800-owrt2410-r28664-01da187749-20250523-1922-newBuildroot.sh
